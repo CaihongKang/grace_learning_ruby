459b5333c7eac8ad74d6397552e84c7ab1d2ef40
Grace.Kang <44062604+CaihongKang@users.noreply.github.com>
2022-09-23 22:44:01 -0500

    add note

--- other_than_ruby_week1.rb
+++ other_than_ruby_week1.rb
@@ -1,5 +1,15 @@
 # @todo please sign up github account, create a git repository grace_learning_ruby, push all your code to that repository
+# https://www.w3schools.com/git/git_intro.asp?remote=github
+# Manage projects with Repositories
+# Clone a project to work on a local copy
+# Control and track changes with Staging and Committing
+# Branch and Merge to allow for work on different parts and versions of a project
+# Pull the latest version of the project to a local copy
+# Push local updates to the main project
 
+# other usefule commands
+# mkdir myproject
+# cd myproject
 # learn below commands
 # git init .
 # $ git init
@@ -7,17 +17,22 @@
 
 
 # git add -A 把项目中的所有文件都放到仓库中暂存区
-# git commit
-# git log
-# git push
+# git commit -m 将修改完成的代码从stage 提交到库中
+# git log  => view the history of commits for a repository
+# git help --all =>all git possible commands
+
 # # git clone
-# git branch
-# git status
+# git branch => create a new branch hello-world-image
+# # git checkout otherbranch_name =>switch from the current branch, to the one specified at the end of the command
+# # git checkout -b emergency-fix =>have created a new branch from master, and changed to it.
+# branch -d branch_name => 删错分支
+# git status 查看状态 git status --short
 # git diff
 # git diff --cached
-# git checkout
+
 # git reset
 # git remote -v
-# git remote add
-# git merge
+# git remote add => dding a remote repository, with the specified URL, as an origin to your local Git repo.
+# # git push  => push our master branch to the origin url(remote repo)
+# git merge =>分支merge到maste branch
 # https://product.hubspot.com/blog/git-and-github-tutorial-for-beginners
